fe546b1d5cdad0a7d6ed7137c09aa1c78ad24f69
Daniel <daniel-cabral14@hotmail.com>
2025-08-26 22:05:48 -0300

    build: initial commit of clean project structure

---
+++ src/main/java/br/com/dunnas/desafio/sistemaaluguel/SistemaAluguelLivrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.dunnas.desafio.sistemaaluguel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAluguelLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAluguelLivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dunnas/desafio/sistemaaluguel/SistemaAluguelLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dunnas.desafio.sistemaaluguel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAluguelLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
